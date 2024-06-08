00266a6732107f3ce80b3a17aafd7a2923fc6423
stendavidson <95745725+stendavidson@users.noreply.github.com>
2024-06-08 22:11:49 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState}  from "react";
+
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Main from "./Components/Main/Main";
+
+import NavigationContext from './Contexts/NavigationContext';
+import SearchContext from './Contexts/SearchContext';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [page, setPage] = useState("Homepage");
+    const [displayState, setDisplayState] = useState("none");
+
+    return (
+        <SearchContext.Provider value={{displayState : displayState, setDisplayState : setDisplayState, siteSearch : () => {console.log("temp")}, results : []}}>
+            <NavigationContext.Provider value={{page : page, setPage : setPage}}>
+                <Header/>
+                <Main page={page}/>
+                <Footer/>
+            </NavigationContext.Provider>
+        </SearchContext.Provider>
+    );
 }
 
 export default App;
