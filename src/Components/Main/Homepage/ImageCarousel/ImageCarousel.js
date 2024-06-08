d192f2eb282e2ecb1fea78c53f43ade5c1bd18f0
stendavidson <95745725+stendavidson@users.noreply.github.com>
2024-06-08 22:01:04 +1000

    Implemented Image Carousel Component

---
+++ src/Components/Main/Homepage/ImageCarousel/ImageCarousel.js
@@ -0,0 +1,18 @@
+import styles from "./ImageCarousel.module.scss"
+
+
+/**
+ * This function renders images into a carousel to the homepage
+ * 
+ * @param {Array} images an array of headings and images to render in the carousel.
+ */
+function ImageCarousel({images}){
+
+    return (
+        <section className={styles.carouselContainer}>
+            
+        </section>
+    );
+}
+
+export default ImageCarousel;
