612f23cb0be585e66b357e00c4d269eea1a891d6
stendavidson <95745725+stendavidson@users.noreply.github.com>
2024-06-08 21:13:57 +1000

    Implemented the footer bar component

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,27 @@
+import styles from "./Footer.module.scss";
+
+import InfoBlock from "./InfoBlock/InfoBlock";
+import LinkBlock from "./LinkBlock/LinkBlock";
+
+
+function Footer(){
+
+    // The primary pages
+    const MAIN_PAGES = ["News", "Fixture", "Ladder", "Highlights"];
+
+    // The secondary pages
+    const SECONDARY_PAGES = ["Youth Leagues", "Contact Us", "Privacy Statement", "Terms & Conditions"];
+
+    return(
+        <footer className={styles.foot}>
+            <div className={styles.footerBox}>
+                <InfoBlock/>
+                <LinkBlock blockTitle="Main Pages" pages={MAIN_PAGES}/>
+                <LinkBlock blockTitle="Other Pages" pages={SECONDARY_PAGES}/>
+            </div>
+            <p className={styles.copyrightNotice}>© 2024 Canberra Football Club</p>
+        </footer>
+    )
+}
+
+export default Footer;
