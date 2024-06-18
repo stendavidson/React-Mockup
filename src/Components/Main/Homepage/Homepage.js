f6a3e9bf197643d2e8238fcf7183f36182d14d94
stendavidson <95745725+stendavidson@users.noreply.github.com>
2024-06-19 05:39:06 +1000

    Add images to carousel

--- src/Components/Main/Homepage/Homepage.js
+++ src/Components/Main/Homepage/Homepage.js
@@ -4,6 +4,11 @@ import AboutUs from "./AboutUs/AboutUs";
 import Weather from "./Weather/Weather";
 import Map from "./Map/Map";
 
+import image1 from "../../../assets/images/Soccer Highlights 1.png";
+import image2 from "../../../assets/images/Soccer Highlights 2.png";
+import image3 from "../../../assets/images/Soccer Highlights 3.png";
+import image4 from "../../../assets/images/Soccer Highlights 4.png";
+import image5 from "../../../assets/images/Soccer Highlights 5.png";
 
 /**
  * This function renders the homepage to the screen.
@@ -12,13 +17,38 @@ function Homepage(){
 
     return (
         <>
-            <ImageCarousel/>
-            <Promotions/>
+            <ImageCarousel images={[
+                {
+                    path : image1,
+                    alt : "Lorem ipsum dolor sit amet",
+                    caption : "Lorem ipsum consectetur adipiscing elit"
+                },
+                {
+                    path : image2,
+                    alt : "Lorem ipsum turpis leo rhoncus libero",
+                    caption : "Lorem ipsum nibh vel posuere vehicula"
+                },
+                {
+                    path : image3,
+                    alt : "Lorem ipsum efficitur sit amet felis",
+                    caption : "Lorem ipsum viverra est ut lorem"
+                },
+                {
+                    path : image4,
+                    alt : "Lorem ipsum egestas cursus libero a fringilla",
+                    caption : "Lorem ipsum malesuada fames ac turpis egestas"
+                },
+                {
+                    path : image5,
+                    alt : "Lorem ipsum in auctor enim consectetur a",
+                    caption : "Lorem ipsum convallis eu condimentum non"
+                }]}/>
+            <Promotions promotions={[]}/>
             <AboutUs/>
             <Weather/>
             <Map/>
         </>
-    )
+    );
 }
 
 export default Homepage;
