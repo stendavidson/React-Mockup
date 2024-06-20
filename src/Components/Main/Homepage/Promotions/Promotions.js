578d1d943c139379c1e39b3aca8df608622f5d62
stendavidson <95745725+stendavidson@users.noreply.github.com>
2024-06-21 04:20:28 +1000

    Fix gap offset issue

--- src/Components/Main/Homepage/Promotions/Promotions.js
+++ src/Components/Main/Homepage/Promotions/Promotions.js
@@ -43,14 +43,14 @@ function Promotions({promotions}){
         }
 
         // Conditional will essentially loop the carousel while transitioning.
-        if(positive && (pos/-590 === (promotions.length - 1))){
+        if(positive && (pos/-595 === (promotions.length - 1))){
             setPos(0);
         }else if(!positive && pos === 0){
-            setPos(-590 * (promotions.length - 1));
+            setPos(-595 * (promotions.length - 1));
         }else if(!positive){
-            setPos(pos + 590);
+            setPos(pos + 595);
         }else{
-            setPos(pos - 590);
+            setPos(pos - 595);
         }
     }
 
